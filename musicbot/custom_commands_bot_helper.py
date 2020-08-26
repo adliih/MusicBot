961e175845f7dcb8d410b0c581c8fac664ec381a
Adli <adli@mamiteam.com>
2020-08-26 14:27:32 +0700

    change log to info

--- musicbot/custom_commands_bot_helper.py
+++ musicbot/custom_commands_bot_helper.py
@@ -18,7 +18,7 @@ def load_custom_command(bot, reload=True):
         if isinstance(custom_command, types.FunctionType):
             function_name = custom_command.__name__
             if function_name.startswith('cmd_'):
-                log.debug("[Custom Method] Binding custom method {}".format(function_name))
+                log.info("[Custom Method] Binding custom method {}".format(function_name))
                 # Add those method to this object
                 setattr(bot, function_name, types.MethodType(custom_command, bot))
 
@@ -36,7 +36,7 @@ def redownload_config():
         config_repo = g.get_repo(os.getenv('GITHUB_CONFIG_REPO'))
         for content in config_repo.get_contents('config'):
             # Copying all files to in folder
-            log.debug('copying ' + content.path)
+            log.info('copying ' + content.path)
             with open(content.path, 'wb') as config_file:
                 print(content.decoded_content)
                 config_file.write(content.decoded_content)
@@ -50,7 +50,7 @@ async def sync_with_config_repo(path):
     load_dotenv(find_dotenv())
 
     if 'GITHUB_TOKEN' in os.environ and 'GITHUB_CONFIG_REPO' in os.environ:
-        log.debug('Will Sync With Config repo: ' + path)
+        log.info('Will Sync With Config repo: ' + path)
         g = Github(os.getenv('GITHUB_TOKEN'))
         config_repo = g.get_repo(os.getenv('GITHUB_CONFIG_REPO'))
         with open(path, 'rb') as path_file:
@@ -58,7 +58,7 @@ async def sync_with_config_repo(path):
                 # Try update
                 existing_content = config_repo.get_contents(path) # this will raise UnknownObjectException if not exist yet
                 config_repo.update_file(path, 'Auto Sync', path_file, existing_content.sha)
-                log.debug('Auto Sync done ' + path)
+                log.info('Auto Sync done ' + path)
             except UnknownObjectException:
                 config_repo.create_file(path, 'Auto Create', path_file)
-                log.debug('Auto Create done ' + path)
+                log.info('Auto Create done ' + path)
